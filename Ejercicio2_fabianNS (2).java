977bd26a82d598dfed96b84aa6d37a36d189d26c
fabiansuarez0114 <77751098+fabiansuarez0114@users.noreply.github.com>
2021-09-19 18:32:50 -0500

    Add files via upload

---
+++ Ejercicio2_fabianNS (2).java
@@ -0,0 +1,23 @@
+
+package ejercicio2_fabianns;
+
+
+public class Ejercicio2_fabianNS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int N = 5;
+ double A = 4.56;
+ char C = 'a';
+ System.out.println("Variable N = " + N);
+ System.out.println("Variable A = " + A);
+ System.out.println("Variable C = " + C);
+ System.out.println(N + " + " + A + " = " + (N+A));
+ System.out.println(A + " - " + N + " = " + (A-N));
+ System.out.println("Valor numérico del carácter " + C + " = " + (int)C); 
+    }
+    
+}
